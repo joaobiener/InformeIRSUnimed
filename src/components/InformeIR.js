5fbea2c1b925505fea0830f6875a600cfe4cf873
joaobiener <joaobiener@hotmail.com>
2022-11-25 10:26:48 -0300

    Ajustes pdf layout

--- src/components/InformeIR.js
+++ src/components/InformeIR.js
@@ -1,9 +1,9 @@
 import { useLocation } from "react-router-dom";
 import useFetch from '../useFetch.js';
 import { useNavigate } from "react-router-dom";
-import React, {useRef} from "react";
-import {useReactToPrint} from 'react-to-print';
-
+import React, { useRef } from "react";
+import { useReactToPrint } from 'react-to-print';
+import Logo from '../layout_set_logo.png'
 
 const InformeIR = () =>
 {
@@ -70,22 +70,22 @@ const InformeIR = () =>
   }
 
 
-  if (loading) return
+  if (!data) return
   (
-    <>
 
-      <div className="container mx-auto mt-5 px-4">
 
-        <div role="status">
-          <svg className="inline mr-2 w-8 h-8 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor" />
-            <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill" />
-          </svg>
-          <span className="sr-only">carregando...</span>
-        </div>
-      </div>;
-    </>
-  )
+    <div className="container mx-auto mt-10 px-4">
+
+      <div>
+        <svg className="inline mr-2 w-8 h-8 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor" />
+          <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill" />
+        </svg>
+        <span className="sr-only">carregando...</span>
+      </div>
+    </div>
+
+  );
 
 
   if (data)
@@ -104,12 +104,31 @@ const InformeIR = () =>
           <div className="overflow-x-auto relative shadow-md sm:rounded-lg mb-5">
             <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
               <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+              <tr>
+                  <th colspan="1" scope="col" className="py-3 px-6 ">
+                  <img src={Logo}/>
+                  </th>
+                  <th colspan="1" scope="col" className="py-3 px-6 ">
+                  <span className="inline uppercase">
+                        Unimed São Gonçalo/Niteroi 
+                    <br/>Cooperativa de trabalho médico 
+                    <br/>Rua Dr. Borman 51 Centro - CEP: 24020-320 - NITEROI-RJ
+                    <br/>CPNJ 28.630.531/0001-87 - INSC. ESTADUAL ISENTO
+                  </span>
+                  </th>
+                  <th colspan="1" scope="col" className="py-3 px-4 ">
+                   ANS - Nº 34.3731
+                  </th>
+                </tr>
                 <tr>
                   <th scope="col" className="py-3 px-6 ">
-                    Informe de Pagamento -  <span className="text-red-800">{data[0].anoReferencia}</span>
+                  
                   </th>
                   <th scope="col" className="py-3 px-6 ">
-
+                  Informe de Pagamento -  <span className="text-red-800">{data[0].anoReferencia}</span>
+                  </th>
+                  <th scope="col" className="py-3 px-4 ">
+                
                   </th>
                 </tr>
               </thead>
@@ -143,123 +162,123 @@ const InformeIR = () =>
             </table>
           </div>
 
-        
+
           {temPagamentos &&
-          <>
-            <h3 className="px-5">Usuários:</h3>
-            <div className="flex flex-col">
-              <div className="overflow-x-auto sm:-mx-6 lg:-mx-8">
-                <div className="py-2 inline-block min-w-full sm:px-6 lg:px-8">
-                  <div className="overflow-hidden">
-                    <table className="min-w-full">
-                      <thead className="bg-white border-b">
+            <>
+              <h3 className="px-5">Usuários:</h3>
+              <div className="flex flex-col">
+                <div className="overflow-x-auto sm:-mx-6 lg:-mx-8">
+                  <div className="py-2 inline-block min-w-full sm:px-4 lg:px-4">
+                    <div className="overflow-hidden">
+                      <table className="min-w-full">
+                        <thead className="bg-white border-b">
 
-                        
-                        <tr>
 
-                          <th scope="col" className="text-sm font-medium text-gray-900 px-6 py-4 text-left">
-                            CPF
-                          </th>
-                          <th scope="col" className="text-sm font-medium text-gray-900 px-6 py-4 text-left">
-                            Data de Inclusão
-                          </th>
-                          <th scope="col" className="text-sm font-medium text-gray-900 px-6 py-4 text-left">
-                            Grau de Parantesco
-                          </th>
-                          <th scope="col" className="text-sm font-medium text-gray-900 px-6 py-4 text-left">
-                            Nome
-                          </th>
-                          <th scope="col" className="text-sm font-medium text-gray-900 px-6 py-4 text-left">
-                            Valor Pago
-                          </th>
-                        </tr>
-                      </thead>
-                      <tbody>
-                        {data.filter(data => data.tipoRegisto <= 2).map((pagamento, index) => (
+                          <tr>
 
-                          <tr key={index} className="border-b transition duration-300 ease-in-out hover:bg-gray-100">
-
-                            <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
-                              {formataCPF(pagamento.documentoBenefiario)}
-                            </td>
-                            <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
-                              {pagamento.dataInclusaoBeneficiario.substring(0, 10)}
-                            </td>
-                            <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
-                              {pagamento.tipoDependencia}
-                            </td>
-                            <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
-                              {pagamento.nomeBeneficiario}
-                            </td>
-                            <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap text-right">
-                              {formateCurrency(pagamento.valorInforme)}
-                            </td>
-
+                            <th scope="col" className="text-sm font-medium text-gray-900 px-6 py-4 text-left">
+                              CPF
+                            </th>
+                            <th scope="col" className="text-sm font-medium text-gray-900 px-6 py-4 text-left">
+                              Data de Inclusão
+                            </th>
+                            <th scope="col" className="text-sm font-medium text-gray-900 px-6 py-4 text-left">
+                              Grau de Parantesco
+                            </th>
+                            <th scope="col" className="text-sm font-medium text-gray-900 px-6 py-4 text-left">
+                              Nome
+                            </th>
+                            <th scope="col" className="text-sm font-medium text-gray-900 px-6 py-4 text-right">
+                              Valor Pago
+                            </th>
                           </tr>
+                        </thead>
+                        <tbody>
+                          {data.filter(data => data.tipoRegisto <= 2).map((pagamento, index) => (
 
-                        ))}
-                      </tbody>
-                    </table>
+                            <tr key={index} className="border-b transition duration-300 ease-in-out hover:bg-gray-100">
+
+                              <td className="text-xs text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                                {formataCPF(pagamento.documentoBenefiario)}
+                              </td>
+                              <td className="text-xs text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                                {pagamento.dataInclusaoBeneficiario.substring(0, 10)}
+                              </td>
+                              <td className="text-xs text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                                {pagamento.tipoDependencia}
+                              </td>
+                              <td className="text-xs text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                                {pagamento.nomeBeneficiario}
+                              </td>
+                              <td className="text-xs text-gray-900 font-light px-6 py-4 whitespace-nowrap text-right">
+                                {formateCurrency(pagamento.valorInforme)}
+                              </td>
+
+                            </tr>
+
+                          ))}
+                        </tbody>
+                      </table>
+                    </div>
                   </div>
                 </div>
               </div>
-            </div>
             </>
           }
           {temReembolso &&
-          <>
-            <h3 className="px-5 py-4">Reembolso:</h3>
-            <div className="flex flex-col">
-              <div className="overflow-x-auto sm:-mx-6 lg:-mx-8">
-                <div className="py-2 inline-block min-w-full sm:px-6 lg:px-8">
-                  <div className="overflow-hidden">
-                    <table className="min-w-full">
-                      <thead className="bg-white border-b">
-                        <tr>
+            <>
+              <h3 className="px-5 py-4">Reembolso:</h3>
+              <div className="flex flex-col">
+                <div className="overflow-x-auto sm:-mx-6 lg:-mx-8">
+                  <div className="py-2 inline-block min-w-full sm:px-6 lg:px-8">
+                    <div className="overflow-hidden">
+                      <table className="min-w-full">
+                        <thead className="bg-white border-b">
+                          <tr>
 
-                          <th scope="col" className="text-sm font-medium text-gray-900 px-6 py-4 text-left">
-                            Matricula
-                          </th>
-                          <th scope="col" className="text-sm font-medium text-gray-900 px-6 py-4 text-left">
-                            Valor Reembolsado
-                          </th>
+                            <th scope="col" className="text-sm font-medium text-gray-900 px-6 py-4 text-left">
+                              Matricula
+                            </th>
+                            <th scope="col" className="text-sm font-medium text-gray-900 px-6 py-4 text-right">
+                              Valor Reembolsado
+                            </th>
 
-                          <th scope="col" className="text-sm font-medium text-gray-900 px-6 py-4 text-left">
-                            Emissor
-                          </th>
-                          <th scope="col" className="text-sm font-medium text-gray-900 px-6 py-4 text-left">
-                            CPF/CNPJ
-                          </th>
-                        </tr>
-                      </thead>
-                      <tbody>
-                        {data.filter(data => data.tipoRegisto >= 3).map((reembolso, index) => (
-
-                          <tr className="border-b transition duration-300 ease-in-out hover:bg-gray-100">
-
-                            <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
-                              {reembolso.codigoCartaoBeneficiario}
-                            </td>
-                            <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap text-right">
-                              {formateCurrency(reembolso.valorInforme)}
-                            </td>
-                            <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
-                              {reembolso.nomeBeneficiario}
-                            </td>
-                            <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
-                              {reembolso.documentoBenefiario}
-                            </td>
-
-
+                            <th scope="col" className="text-sm font-medium text-gray-900 px-6 py-4 text-left">
+                              Emissor
+                            </th>
+                            <th scope="col" className="text-sm font-medium text-gray-900 px-6 py-4 text-left">
+                              CPF/CNPJ
+                            </th>
                           </tr>
+                        </thead>
+                        <tbody>
+                          {data.filter(data => data.tipoRegisto >= 3).map((reembolso, index) => (
 
-                        ))}
-                      </tbody>
-                    </table>
+                            <tr className="border-b transition duration-300 ease-in-out hover:bg-gray-100">
+
+                              <td className="text-xs text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                                {reembolso.codigoCartaoBeneficiario}
+                              </td>
+                              <td className="text-xs text-gray-900 font-light px-6 py-4 whitespace-nowrap text-right">
+                                {formateCurrency(reembolso.valorInforme)}
+                              </td>
+                              <td className="text-xs text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                                {reembolso.nomeBeneficiario}
+                              </td>
+                              <td className="text-xs text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                                {reembolso.documentoBenefiario}
+                              </td>
+
+
+                            </tr>
+
+                          ))}
+                        </tbody>
+                      </table>
+                    </div>
                   </div>
                 </div>
               </div>
-            </div>
             </>
           }
         </div>
